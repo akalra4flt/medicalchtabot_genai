8328a68ed0d6c9fc8d91cc48a7ef6a0ff7655dbe
amitkalra1204 <kumaramit.stats@gmail.com>
2026-09-06 19:18:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author='Amit',
+    author_email="akalra4flt@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+
+
+)
